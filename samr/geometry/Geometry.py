986fe94990935edf45bdab17cfad254e18d104e6
Kevin Chu <kevin@velexi.com>
2018-09-10 15:05:28 -0700

    Add skeleton implementation of 'geometry' package.

---
+++ samr/geometry/Geometry.py
@@ -0,0 +1,59 @@
+"""
+TODO: add docstring
+
+------------------------------------------------------------------------------
+COPYRIGHT/LICENSE.  This file is part of the XYZ package.  It is subject
+to the license terms in the LICENSE file found in the top-level directory of
+this distribution.  No part of the XYZ package, including this file, may be
+copied, modified, propagated, or distributed except according to the terms
+contained in the LICENSE file.
+------------------------------------------------------------------------------
+"""
+# pylint: disable=invalid-name
+
+# --- Imports
+
+# Standard library
+import doctest
+
+# XYZ
+# TODO
+
+# --- Constants
+
+
+# --- Class definition
+
+class Geometry:
+    """
+    TODO
+    """
+    # --- Properties
+
+    # --- Public methods
+
+    def __init__(self):
+        """
+        TODO
+
+        Parameters
+        ----------
+        TODO
+
+        Examples
+        --------
+        TODO
+        """
+        # --- Check arguments
+
+        # --- Set property and attribute values
+
+        # PYLINT: eliminate 'defined outside __init__' error
+        pass
+
+
+# --- Main program
+
+if __name__ == '__main__':
+    # Run doctests
+    doctest.testmod()
