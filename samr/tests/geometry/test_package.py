b90e8506ebbf38c0fd06890b95b4b7a462bc98cc
Kevin Chu <kevin@velexi.com>
2018-09-10 16:16:24 -0700

    Add initial unit tests for 'geometry' package.

---
+++ samr/tests/geometry/test_package.py
@@ -0,0 +1,26 @@
+"""
+Unit tests for 'geometry' package.
+
+------------------------------------------------------------------------------
+COPYRIGHT/LICENSE.  This file is part of the XYZ package.  It is subject
+to the license terms in the LICENSE file found in the top-level directory of
+this distribution.  No part of the XYZ package, including this file, may be
+copied, modified, propagated, or distributed except according to the terms
+contained in the LICENSE file.
+------------------------------------------------------------------------------
+"""
+# --- Imports
+
+# XYZ
+from samr import geometry
+
+
+# --- Tests
+
+def test_attributes():
+    """
+   Test for expected package attributes.
+    """
+    # Package information
+    assert geometry.MeshGeometry
+    assert geometry.BlockGeometry
