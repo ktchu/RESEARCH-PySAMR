ea7cfffefe1f573f88d594eb01ae223e595c66e5
Kevin Chu <kevin@velexi.com>
2018-09-10 23:37:53 -0700

    Add initial implementation for geometry classes.

--- samr/geometry/BlockGeometry.py
+++ samr/geometry/BlockGeometry.py
@@ -14,7 +14,9 @@ contained in the LICENSE file.
 # --- Imports
 
 # Standard library
-import doctest
+
+# External packages
+import numpy
 
 # XYZ
 # TODO
@@ -30,15 +32,43 @@ class BlockGeometry:
     """
     # --- Properties
 
+    @property
+    def num_dimensions(self):
+        """
+        int: number of spatial dimensions
+        """
+        return self._num_dimensions
+
+    @property
+    def x_lower(self):
+        """
+        numpy.array: lower corner of MeshBlock
+        """
+        return self._x_lower
+
+    @property
+    def dx(self):
+        """
+        numpy.array: grid spacing on MeshBlock
+        """
+        return self._dx
+
     # --- Public methods
 
-    def __init__(self):
+    def __init__(self, num_dimensions, x_lower, dx):
         """
         TODO
 
         Parameters
         ----------
-        TODO
+        num_dimensions: int
+            number of spatial dimensions
+
+        x_lower: numpy.array
+            lower corner of MeshBlock
+
+        dx: numpy.array
+            grid spacing on MeshBlock
 
         Examples
         --------
@@ -46,7 +76,31 @@ class BlockGeometry:
         """
         # --- Check arguments
 
+        # num_dimensions
+        if not isinstance(num_dimensions, int):
+            raise ValueError("'num_dimensions' is not an integer")
+
+        if num_dimensions <= 0:
+            raise ValueError("'num_dimensions' is not a positive integer")
+
+        # x_lower
+        if not isinstance(x_lower, numpy.array):
+            raise ValueError("'x_lower' is not a numpy.array")
+
+        if len(x_lower) != num_dimensions:
+            err_msg = "'x_lower' does not have 'num_dimensions' components"
+            raise ValueError(err_msg)
+
+        # dx
+        if not isinstance(dx, numpy.array):
+            raise ValueError("'dx' is not a numpy.array")
+
+        if len(dx) != num_dimensions:
+            err_msg = "'dx' does not have 'num_dimensions' components"
+            raise ValueError(err_msg)
+
         # --- Set property and attribute values
 
-        # PYLINT: eliminate 'defined outside __init__' error
-        pass
+        self._num_dimensions = num_dimensions
+        self._x_lower = x_lower
+        self._dx = dx
--- samr/geometry/MeshGeometry.py
+++ samr/geometry/MeshGeometry.py
@@ -14,7 +14,9 @@ contained in the LICENSE file.
 # --- Imports
 
 # Standard library
-import doctest
+
+# External packages
+import numpy
 
 # XYZ
 # TODO
@@ -27,18 +29,49 @@ import doctest
 class MeshGeometry:
     """
     TODO
+
+    * logically rectangular region of space
+    * uniform grid spacing in each direction
     """
     # --- Properties
 
+    @property
+    def num_dimensions(self):
+        """
+        int: number of spatial dimensions
+        """
+        return self._num_dimensions
+
+    @property
+    def x_lower(self):
+        """
+        numpy.array: lower corner of mesh on coarsest level
+        """
+        return self._x_lower
+
+    @property
+    def dx(self):
+        """
+        numpy.array: grid spacing on coarsest level
+        """
+        return self._dx
+
     # --- Public methods
 
-    def __init__(self):
+    def __init__(self, num_dimensions, x_lower, dx):
         """
         TODO
 
         Parameters
         ----------
-        TODO
+        num_dimensions: int
+            number of spatial dimensions
+
+        x_lower: numpy.array
+            lower corner of mesh on coarsest level
+
+        dx: numpy.array
+            grid spacing on coarsest level
 
         Examples
         --------
@@ -46,7 +79,31 @@ class MeshGeometry:
         """
         # --- Check arguments
 
+        # num_dimensions
+        if not isinstance(num_dimensions, int):
+            raise ValueError("'num_dimensions' is not an integer")
+
+        if num_dimensions <= 0:
+            raise ValueError("'num_dimensions' is not a positive integer")
+
+        # x_lower
+        if not isinstance(x_lower, numpy.array):
+            raise ValueError("'x_lower' is not a numpy.array")
+
+        if len(x_lower) != num_dimensions:
+            err_msg = "'x_lower' does not have 'num_dimensions' components"
+            raise ValueError(err_msg)
+
+        # dx
+        if not isinstance(dx, numpy.array):
+            raise ValueError("'dx' is not a numpy.array")
+
+        if len(dx) != num_dimensions:
+            err_msg = "'dx' does not have 'num_dimensions' components"
+            raise ValueError(err_msg)
+
         # --- Set property and attribute values
 
-        # PYLINT: eliminate 'defined outside __init__' error
-        pass
+        self._num_dimensions = num_dimensions
+        self._x_lower = x_lower
+        self._dx = dx
