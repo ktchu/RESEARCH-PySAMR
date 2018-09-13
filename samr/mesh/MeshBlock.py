27e3ee42fca2940f30f1d7b19f03749a48cdefe3
Kevin Chu <kevin@velexi.com>
2018-09-12 23:20:03 -0700

    Add argument checks to MeshBlock.__init__()
    * Add unit tests.

--- samr/mesh/MeshBlock.py
+++ samr/mesh/MeshBlock.py
@@ -20,8 +20,13 @@ import numpy
 
 # XYZ
 # TODO
+from samr.geometry import MeshGeometry
 
 # --- Constants
+
+# numpy integer data types
+_NUMPY_INT_DTYPES = [numpy.int_, numpy.intc, numpy.intp,
+                     numpy.int8, numpy.int16, numpy.int32, numpy.int64]
 
 
 # --- Class definition
@@ -31,6 +36,35 @@ class MeshBlock:
     TODO
     """
     # --- Properties
+
+    @property
+    def num_dimensions(self):
+        """
+        int: dimensionality of index space
+        """
+        return self._geometry.num_dimensions
+
+    @property
+    def lower(self):
+        """
+        numpy.ndarray: lower corner of index space covered by MeshBlock
+
+        Notes
+        -----
+        * lower.dtype = 'int64'
+        """
+        return self._lower
+
+    @property
+    def upper(self):
+        """
+        numpy.ndarray: upper corner of index space covered by MeshBlock
+
+        Notes
+        -----
+        * upper.dtype = 'int64'
+        """
+        return self._upper
 
     @property
     def geometry(self):
@@ -49,14 +83,20 @@ class MeshBlock:
 
     # --- Public methods
 
-    def __init__(self, geometry):
+    def __init__(self, geometry, lower, upper):
         """
         TODO
 
         Parameters
         ----------
         geometry: BlockGeometry
-            TODO
+            geometry of MeshBlock
+
+        lower: numpy.ndarray of integers
+            lower corner of index space covered by MeshBlock
+
+        upper: numpy.ndarray of integers
+            upper corner of index space covered by MeshBlock
 
         Examples
         --------
@@ -64,11 +104,47 @@ class MeshBlock:
         """
         # --- Check arguments
 
+        # geometry
+        if not isinstance(geometry, MeshGeometry):
+            raise ValueError("'geometry' is not MeshGeometry object")
+
+        # get dimensionality of geometry
+        num_dimensions = geometry.num_dimensions
+
+        # lower
+        if not isinstance(lower, numpy.ndarray):
+            raise ValueError("'lower' is not a numpy.ndarray")
+
+        if len(lower) != num_dimensions:
+            err_msg = "'lower' does not have 'num_dimensions' components"
+            raise ValueError(err_msg)
+
+        if lower.dtype not in _NUMPY_INT_DTYPES:
+            err_msg = "'lower' does not have an integer dtype"
+            raise ValueError(err_msg)
+
+        # upper
+        if not isinstance(upper, numpy.ndarray):
+            raise ValueError("'upper' is not a numpy.ndarray")
+
+        if len(upper) != num_dimensions:
+            err_msg = "'upper' does not have 'num_dimensions' components"
+            raise ValueError(err_msg)
+
+        if upper.dtype not in _NUMPY_INT_DTYPES:
+            err_msg = "'upper' does not have an integer dtype"
+            raise ValueError(err_msg)
+
         # --- Set property and attribute values
 
         # PYLINT: eliminate 'defined outside __init__' error
         self._data = {}
 
+        # index space
+        self._lower = lower.astype('int64')
+        self._upper = upper.astype('int64')
+
+        # geometry
         self._geometry = geometry
 
     def add_variable(self, mesh_variable):
---
+++ samr/tests/mesh/test_MeshBlock.py
@@ -0,0 +1,157 @@
+"""
+Unit tests MeshBlock class
+
+------------------------------------------------------------------------------
+COPYRIGHT/LICENSE.  This file is part of the XYZ package.  It is subject
+to the license terms in the LICENSE file found in the top-level directory of
+this distribution.  No part of the XYZ package, including this file, may be
+copied, modified, propagated, or distributed except according to the terms
+contained in the LICENSE file.
+------------------------------------------------------------------------------
+"""
+# pylint: disable=invalid-name
+
+# --- Imports
+
+# Standard library
+import unittest
+
+# External packages
+import numpy
+import pytest
+
+# XYZ
+from samr.geometry import MeshGeometry
+from samr.mesh import MeshBlock
+
+
+# --- Tests
+
+class MeshBlockTests(unittest.TestCase):
+    """
+    Unit tests for MeshBlock class.
+    """
+    # --- setUp/tearDown
+
+    def setUp(self):
+        """
+        Set up test fixtures.
+        """
+        self.num_dimensions = 3
+        self.x_lower = numpy.zeros(self.num_dimensions)
+        self.dx = 0.1*numpy.ones(self.num_dimensions)
+        self.geometry = MeshGeometry(self.num_dimensions,
+                                     self.x_lower,
+                                     self.dx)
+
+    # --- Test cases
+
+    @staticmethod
+    def test_attributes():
+        """
+        Test for expected attributes.
+        """
+        # Properties
+        assert hasattr(MeshBlock, 'num_dimensions')
+        assert hasattr(MeshBlock, 'lower')
+        assert hasattr(MeshBlock, 'upper')
+
+    def test_init_1(self):
+        """
+        Test construction of MeshBlock object with valid parameters.
+        """
+        # Exercise functionality
+        lower = numpy.zeros(self.num_dimensions, dtype='int')
+        upper = numpy.ones(self.num_dimensions, dtype='int')
+        block = MeshBlock(self.geometry, lower, upper)
+
+        # Check results
+        assert block.num_dimensions == self.num_dimensions
+        assert numpy.array_equal(block.lower, lower)
+        assert block.lower.dtype == numpy.int64
+        assert numpy.array_equal(block.upper, upper)
+        assert block.upper.dtype == numpy.int64
+
+    def test_init_2(self):
+        """
+        Test construction of MeshBlock object. Invalid 'geometry'
+        """
+        # --- Preparations
+
+        lower = numpy.zeros(self.num_dimensions, dtype='int')
+        upper = numpy.ones(self.num_dimensions, dtype='int')
+
+        # --- Exercise functionality and check results
+
+        # num_dimensions not an int
+        with pytest.raises(ValueError) as exc_info:
+            _ = MeshBlock(geometry='not a MeshGeometry object',
+                          lower=lower, upper=upper)
+
+        if exc_info:
+            expected_error = "'geometry' is not MeshGeometry object"
+        assert expected_error in str(exc_info)
+
+    def test_init_3(self):
+        """
+        Test construction of MeshBlock object. Invalid 'lower'
+        """
+        # --- Preparations
+
+        upper = numpy.ones(self.num_dimensions)
+
+        # --- Exercise functionality and check results
+
+        # lower not a numpy.ndarray
+        with pytest.raises(ValueError) as exc_info:
+            _ = MeshBlock(geometry=self.geometry,
+                          lower=3,
+                          upper=upper)
+
+        if exc_info:
+            expected_error = "'lower' is not a numpy.ndarray"
+        assert expected_error in str(exc_info)
+
+        # len(lower) != num_dimensions
+        with pytest.raises(ValueError) as exc_info:
+            _ = MeshBlock(geometry=self.geometry,
+                          lower=numpy.zeros(self.num_dimensions-1,
+                                            dtype='int'),
+                          upper=upper)
+
+        if exc_info:
+            expected_error = "'lower' does not have 'num_dimensions' " \
+                             "components"
+        assert expected_error in str(exc_info)
+
+    def test_init_4(self):
+        """
+        Test construction of MeshBlock object. Invalid 'upper'
+        """
+        # --- Preparations
+
+        lower = numpy.zeros(self.num_dimensions, dtype='int')
+
+        # --- Exercise functionality and check results
+
+        # upper not a numpy.ndarray
+        with pytest.raises(ValueError) as exc_info:
+            _ = MeshBlock(geometry=self.geometry,
+                          lower=lower,
+                          upper='not a numpy.ndarray')
+
+        if exc_info:
+            expected_error = "'upper' is not a numpy.ndarray"
+        assert expected_error in str(exc_info)
+
+        # len(upper) != num_dimensions
+        with pytest.raises(ValueError) as exc_info:
+            _ = MeshBlock(geometry=self.geometry,
+                          lower=lower,
+                          upper=numpy.ones(self.num_dimensions+1,
+                                           dtype='int'))
+
+        if exc_info:
+            expected_error = "'upper' does not have 'num_dimensions' " \
+                             "components"
+        assert expected_error in str(exc_info)
