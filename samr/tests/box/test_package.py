ace7d4f5fdc4d5b4635cf78d52b26173951ab71f
Kevin Chu <kevin@velexi.com>
2018-10-01 23:29:34 -0700

    Add tests/box/test_package.py

---
+++ samr/tests/box/test_package.py
@@ -0,0 +1,25 @@
+"""
+Unit tests for 'box' package.
+
+------------------------------------------------------------------------------
+COPYRIGHT/LICENSE.  This file is part of the XYZ package.  It is subject
+to the license terms in the LICENSE file found in the top-level directory of
+this distribution.  No part of the XYZ package, including this file, may be
+copied, modified, propagated, or distributed except according to the terms
+contained in the LICENSE file.
+------------------------------------------------------------------------------
+"""
+# --- Imports
+
+# XYZ
+from samr import box
+
+
+# --- Tests
+
+def test_attributes():
+    """
+   Test for expected package attributes.
+    """
+    # Package information
+    assert box.Box
