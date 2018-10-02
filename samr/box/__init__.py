6bb5957566d7bb2de9ef6b42ba3deeb513883a3f
Kevin Chu <kevin@velexi.com>
2018-10-01 23:11:57 -0700

    Move Box to 'box' package. Update unit tests.

---
+++ samr/box/__init__.py
@@ -0,0 +1,18 @@
+"""
+TODO: add docstring
+
+------------------------------------------------------------------------------
+COPYRIGHT/LICENSE.  This file is part of the XYZ package.  It is subject
+to the license terms in the LICENSE file found in the top-level directory of
+this distribution.  No part of the XYZ package, including this file, may be
+copied, modified, propagated, or distributed except according to the terms
+contained in the LICENSE file.
+------------------------------------------------------------------------------
+"""
+# Package types
+from .Box import Box
+
+# Exported packages and modules
+__all__ = [
+    'Box',
+]
