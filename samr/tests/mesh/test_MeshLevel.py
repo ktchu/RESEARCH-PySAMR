4a82b48a11be63bc0de0669f55544eedb3b9c463
Kevin Chu <kevin@velexi.com>
2018-10-03 21:46:11 -0700

    Fix error in comments.

--- samr/tests/mesh/test_MeshLevel.py
+++ samr/tests/mesh/test_MeshLevel.py
@@ -200,7 +200,7 @@ class MeshLevelTests(unittest.TestCase):
 
         # --- Exercise functionality and check results
 
-        # first_box_geometry not a Geometry
+        # 'variable' not a MeshVariable
         with pytest.raises(ValueError) as exc_info:
             level.add_variable('not a MeshVariable')
 
